9b63c5b05266494a9b5abc8cde60ebaa2ba679ae
prasannatalla18 <prasannatalla18@gmail.com>
2026-06-27 12:49:30 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,47 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #indexing
+>>> a="vijayawada"
+>>> a[0]
+'v'
+>>> a[3]
+'a'
+>>> a[5]
+'a'
+>>> a[0]+a[1]+a[2]+a[3]+a[4]+a[5]
+'vijaya'
+>>> a="i am learning python course"
+>>> a[14]+a[15]+a[16]+a[17]+a[18]+a[19]
+'python'
+>>> a[6]+a[7]+a[8]+a[9]+a[10]
+'earni'
+>>> a[5]+a[6]+a[7]+a[8]+a[9]
+'learn'
+>>> a[21]+a[22]+a[23]+a[24]+a[25]+a[26]
+'course'
+>>> b="time is very precious"
+>>> b[13]+b[14]+b[15]+b[16]+b[17]+b[18]+b[19]+b[20]
+'precious'
+>>> b[8]+b[9]+b[10]+b[11]
+'very'
+>>> b[0]+b[1]+b[2]+b[3]
+'time'
+>>> #negative indexing
+>>> a="simple is better than complex"
+>>> a[-29]+a[-28]+a[-27]+a[-26]+a[-25]+a[-24]
+'simple'
+>>> a[-19]+a[-18]+a[-17]+a[-16]+a[-15]+a[-14]
+'better'
+>>> a[-12]+a[-11]+a[-9]+a[-8]
+'thn '
+>>> a[-12]+a[-11]+a[-10]+a[-9]
+'than'
+>>> a[-7]+a[-6]+a[-5]+a[-4]+a[-3]+a[-2]+a[-1]
+'complex'
+>>> b="i love python"
+>>> b[-11]+a[-10]+a[-9]+a[-8]
+'lan '
+>>> b[-11]+b[-10]+b[-9]+b[-8]
+'love'
+>>> b[-6]+b[-5]+b[-4]+b[-3]+b[-2]+b[-1]
+'python'
