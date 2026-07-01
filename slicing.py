ef2f5b7e2dc4af761d383ee66c355d3f6f6d936a
prasannatalla18 <prasannatalla18@gmail.com>
2026-07-01 18:27:32 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,44 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #slicing
+>>> a="codegnan"
+>>> a[0:3]
+'cod'
+>>> a[0:4]
+'code'
+>>> a[4:8]
+'gnan'
+>>> a[:4]
+'code'
+>>> a[4:]
+'gnan'
+>>> b="work until you succeed"
+>>> b[5:10]
+'until'
+>>> b[11:14]
+'you'
+>>> b[:4]
+'work'
+>>> b[15:]
+'succeed'
+>>> c="codegnan it solutions"
+>>> c[9:11]
+'it'
+>>> c[12:20]
+'solution'
+>>> c[:8]
+'codegnan'
+>>> d="vijayawada is a royal city"
+>>> d[-10:-5]
+'royal'
+>>> d[:-16]
+'vijayawada'
+>>> d[-4:]
+'city'
+>>> e="vizag is city of destiny"
+>>> e[-15:-11]
+'city'
+>>> e[:-19]
+'vizag'
+>>> e[-7:]
+'destiny'
